6c18b244f28108ddf85c31a75a9b7809ab7fd7d0
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-02-01 21:07:44 +0300

    fix file upload test

--- file_uploader/processor/tests/test_api.py
+++ file_uploader/processor/tests/test_api.py
@@ -1,8 +1,12 @@
+from unittest.mock import patch
+from pathlib import Path
+
 import pytest
+from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework import status
 
-pytestmark = pytest.mark.django_db
+from ..tasks import process_file
 
 
 @pytest.mark.django_db
@@ -13,8 +17,15 @@ class TestAPI:
         response = client.get('/files/')
         assert response.status_code == status.HTTP_200_OK
 
-    def test_upload_file(self, client):
-        video = SimpleUploadedFile('test.txt', b'file_content', content_type='text/plain')
-        response = client.post('/upload/', data={'file': video}, format='multipart')
-        print(response.data)
+    @patch.object(process_file, 'delay')  # Mock Celery
+    def test_upload_file_201(self, mock_delay, client):
+        file = SimpleUploadedFile('test.txt', b'file_content', content_type='text/plain')
+        response = client.post('/upload/', data={'file': file}, format='multipart')
+        full_path = settings.BASE_DIR / response.data['file'][1:]
         assert response.status_code == status.HTTP_201_CREATED
+        assert full_path.is_file()
+        Path.unlink(full_path)
+
+    def test_upload_file_400(self, client):
+        response = client.post('/upload/', data={'file': ''}, format='multipart')
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
